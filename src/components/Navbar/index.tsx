127bfe9e9862039bd8322a2f712c79bd5c7d867b
VoN <sudo.von.contact@gmail.com>
2021-10-26 21:45:54 -0500

    feat: :sparkles: Add material ui

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+/* Custom components. */
+import { AppBar, Box } from '@mui/material/'
+
+const Navbar = () => {
+  return(
+    <AppBar position='sticky'>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <Box display='flex' alignItems='center'>
+        </Box>
+      </Box>
+    </AppBar>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from '@mui/material';
+
+const Portfolio = () => (
+    <Container>asas</Container>
+)
+
+export default Portfolio

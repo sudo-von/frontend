b30eb02e5fc2d97962d6410cf76cde8f31a0e7f9
VoN <sudo.von.contact@gmail.com>
2021-10-26 21:52:18 -0500

    feat: :sparkles: Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Portfolio from './pages/Portfolio/'
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Portfolio/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App

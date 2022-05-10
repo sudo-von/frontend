f0ef079f852403ba97641041a0c3c559cfc7dfc3
VoN <sudo.von.contact@gmail.com>
2022-05-10 16:40:12 -0500

    feat: :sparkles: Handled component in a better way

--- src/components/Container/Container.styles.tsx
+++ src/components/Container/Container.styles.tsx
@@ -1,5 +1,6 @@
 export const styles = {
   grid: {
+    padding: 10,
     marginTop: 250,
     marginBottom: 250,
   },
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,28 +1,39 @@
 import { Grid } from '@mui/material';
 import { styles } from './Container.styles';
 
-type ContainerProps = {
+export type ContainerProps = {
   children: React.ReactNode;
   spacing?: number;
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  justifyContent?:
+    | 'flex-start'
+    | 'center'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  alignItems?: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline';
 };
 
-const Container = ({ children, spacing }: ContainerProps): JSX.Element => {
+const Container = ({
+  children,
+  spacing = 0,
+  direction = 'row',
+  justifyContent = 'center',
+  alignItems = 'center',
+}: ContainerProps): JSX.Element => {
   return (
     <Grid
       container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
+      direction={direction}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
       spacing={spacing}
       style={styles.grid}
     >
       {children}
     </Grid>
   );
-};
-
-Container.defaultProps = {
-  spacing: 0,
 };
 
 export default Container;
